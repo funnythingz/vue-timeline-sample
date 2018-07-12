0ec9f4d12ff460d16deb95e5e6e9f3d316cff501
funnythingz <funnythingz69+github@gmail.com>
2018-07-12 11:43:23 +0900

    vueでtimelineっぽいものつくった

---
+++ README.md
@@ -0,0 +1,25 @@
+# vue-timeline-sample
+
+- webpack v4
+- babel
+- Vue.js
+
+## command
+
+build mode=development
+
+```
+$ npm run build:dev
+```
+
+build mode=production
+
+```
+$ npm run build:prod
+```
+
+serve
+
+```
+$ npm run start:dev
+```
---
+++ src/components/timeline.vue
@@ -0,0 +1,35 @@
+<template lang="pug">
+.container
+  textarea(v-model="tweet")
+  button(@click="post()")
+    | post
+  .list-group
+    .list-item(v-for="t in tweetList")
+      | {{t}}
+</template>
+
+<style lang="sass">
+</style>
+
+<script>
+import { reverse } from 'lodash'
+
+export default {
+  data() {
+    return {
+      tweet: '',
+      tweetList: []
+    }
+  },
+  methods: {
+    post() {
+      this.tweetList.push(this.tweet)
+      this.tweetList = reverse(this.tweetList)
+      this.clear()
+    },
+    clear() {
+      this.tweet = ''
+    },
+  }
+}
+</script>
